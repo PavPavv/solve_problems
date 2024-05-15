40754ce0d81e4064b3dd1f84d6828877c9e87ce2
PavPavv <pavpavdev@gmail.com>
2024-05-16 00:19:23 +0400

    feat: add another python solution for with_separator problem

--- python/easy/with_separator.py
+++ python/easy/with_separator.py
@@ -4,3 +4,8 @@ def with_separator(separator, *args):
     result += separator + format(arg)
   return result
 print(with_separator('!',1,2,3,4,5))
+
+def with_separator_1(args):
+  separator = args[0]
+  return separator + separator.join([f"{value}" for value in args[1:]])
+print(with_separator_1(['$',1,2,3,4]))
