4ce53dc6723e3f8080c64ffc01a82ff3e41910d4
PavPavv <pavpavdev@gmail.com>
2024-07-08 13:11:13 +0400

    feat: add another find black sheep solution

--- python/medium/find_black_sheep.py
+++ python/medium/find_black_sheep.py
@@ -13,3 +13,38 @@ def find_black_sheep(arr):
     return even[0]
 
 print(find_black_sheep([2,4,6,8,9,10,12]))
+
+class Finder():
+    def __init__(self, l):
+        self.l = l
+
+    def _is_l(self, l):
+        if not isinstance(l, list):
+            return False
+        else:
+            return True
+
+    def _is_even_l(self, l):
+        if self._is_l(l):
+            first_three = l[0:3]
+            r = list(filter(lambda item: item % 2 == 0, first_three))
+            if len(r) > 1:
+                return True
+            else:
+                return False
+        else:
+            raise TypeError('Error! Must be a list!')
+
+    def black_sheep(self):
+        is_even = self._is_even_l(self.l)
+        if is_even:
+            return list(filter(lambda item: item % 2 != 0, self.l))[0]
+        else:
+            return list(filter(lambda item: item % 2 == 0, self.l))[0]
+            
+test = [2, 4, 6, 8, 9, 10, 12]
+test1 = [1, 3, 5, 7, 8, 9, 11]
+finder = Finder(test)
+finder1 = Finder(test1)
+print(finder.black_sheep())
+print(finder1.black_sheep())
--- ts/medium/findBlackSheep.ts
+++ ts/medium/findBlackSheep.ts
@@ -6,3 +6,15 @@ export const findBlackSheep = (arr: number[]): number => {
 };
 
 console.log(findBlackSheep([2, 4, 6, 8, 9, 10, 12]));
+
+const findBlackSheep2 = (arr: number[]): number => {
+    const isArrEven = arr.slice(0,3).filter((item) => item % 2 === 0).length > 1;
+    if (isArrEven) {
+        return arr.find((item) => item % 2 !== 0) ?? 0;
+    } else {
+        return arr.find((item) => item % 2 === 0) ?? 0;
+    }
+};
+console.log(findBlackSheep2([2, 4, 6, 8, 9, 10, 12]));  // 9
+console.log(findBlackSheep2([1, 2, 3, 5, 7, 9, 11]));  // 2
+
