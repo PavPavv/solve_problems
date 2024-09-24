cee6ba529641fbd541457a528f3ea6c3cce63820
PavPavv <pavpavdev@gmail.com>
2024-09-24 19:02:37 +0400

    feat: reduce method for add tree generator class example

--- ts/medium/treeGenerator.ts
+++ ts/medium/treeGenerator.ts
@@ -1,130 +1,146 @@
 type MyNode = {
-  id: number;
-  text: string;
-  children: MyNode[];
+    id: number;
+    text: string;
+    children: MyNode[];
 }
 
 const data: MyNode[] = [
-  {
-      id: 1,
-      text: 'one',
-      children: [],
-  },
-  {
-      id: 2,
-      text: 'two',
-      children: [],
-  },
-  {
-      id: 3,
-      text: 'three',
-      children: [
-          {
-              id: 6,
-              text: 'six',
-              children: [
-                  {
-                      id: 10,
-                      text: 'ten',
-                      children: [],
-                  },
-              ],
-          },
-          {
-              id: 7,
-              text: 'seven',
-              children: [
-                  {
-                      id: 11,
-                      text: 'eleven',
-                      children: [],
-                  },
-              ],
-          },
-          {
-              id: 8,
-              text: 'eight',
-              children: [
-                  {
-                      id: 12,
-                      text: 'twelve',
-                      children: [
-                          {
-                              id: 13,
-                              text: 'thirteen',
-                              children: [],
-                          },
-                          {
-                              id: 14,
-                              text: 'fourteen',
-                              children: [],
-                          },
-                          {
-                              id: 15,
-                              text: 'fifteen',
-                              children: [],
-                          },
-                      ],
-                  },
-              ],
-          },
-                      {
-              id: 9,
-              text: 'nine',
-              children: [],
-          },
-      ],
-  },
-  {
-      id: 4,
-      text: 'four',
-      children: [],
-  },
-  {
-      id: 5,
-      text: 'five',
-      children: [],
-  },
+    {
+        id: 1,
+        text: 'one',
+        children: [],
+    },
+    {
+        id: 2,
+        text: 'two',
+        children: [],
+    },
+    {
+        id: 3,
+        text: 'three',
+        children: [
+            {
+                id: 6,
+                text: 'six',
+                children: [
+                    {
+                        id: 10,
+                        text: 'ten',
+                        children: [],
+                    },
+                ],
+            },
+            {
+                id: 7,
+                text: 'seven',
+                children: [
+                    {
+                        id: 11,
+                        text: 'eleven',
+                        children: [],
+                    },
+                ],
+            },
+            {
+                id: 8,
+                text: 'eight',
+                children: [
+                    {
+                        id: 12,
+                        text: 'twelve',
+                        children: [
+                            {
+                                id: 13,
+                                text: 'thirteen',
+                                children: [],
+                            },
+                            {
+                                id: 14,
+                                text: 'fourteen',
+                                children: [],
+                            },
+                            {
+                                id: 15,
+                                text: 'fifteen',
+                                children: [],
+                            },
+                        ],
+                    },
+                ],
+            },
+                        {
+                id: 9,
+                text: 'nine',
+                children: [],
+            },
+        ],
+    },
+    {
+        id: 4,
+        text: 'four',
+        children: [],
+    },
+    {
+        id: 5,
+        text: 'five',
+        children: [],
+    },
 ];
 
 class TreeCode {
-  constructor(
-      public id: number,
-      public text: string,
-      public children: MyNode[],
-  ) {}
+    constructor(
+        public id: number,
+        public text: string,
+        public children: MyNode[],
+    ) {}
 }
 
 class TreeConvertor {
-  private _tree: TreeCode[] = [];
-  private _data: MyNode[] = [];
+    private _tree: TreeCode[] = [];
+    private _data: MyNode[] = [];
 
-  constructor(data: MyNode[]) {
-      this._data = data;
-  }
+    constructor(data: MyNode[]) {
+        this._data = data;
+    }
 
-  private _makeTree(array: MyNode[]): TreeCode[]  {
-      const tree: TreeCode[] = [];
-      for (let i = 0; i < array.length; i++) {
-          const item = array[i];
-          const  newTree = new TreeCode(item.id, item.text, []));
-          if (item.children.length) {
-              newTree.children = this._makeTree(item.children);
-          }
-          tree.push(newTree);
-      }
-      return tree;
-  }
+    private _makeTree(array: MyNode[]): TreeCode[]  {
+        const tree: TreeCode[] = [];
+        for (let i = 0; i < array.length; i++) {
+            const item = array[i];
+            const  newTree = new TreeCode(item.id, item.text, []));
+            if (item.children.length) {
+                newTree.children = this._makeTree(item.children);
+            }
+            tree.push(newTree);
+        }
+        return tree;
+    }
 
-  public setTree(): void {
-      this._tree = this._makeTree(this._data);
-  }
+    private _makeTreeReduced(array: MyNode[]): TreeCode[]  {
+        return array.reduce((tree: TreeCode[], node: MyNode) => {
+            const newTree = new TreeCode(node.id, node.text, []);
+            if (node.children.length) {
+                newTree.children = this._makeTreeReduced(node.children);
+            }
+            tree.push(newTree);
+            return tree;
+        }, []);
+    }
 
-  public getTree(): TreeCode[] {
-      return this._tree;
-  }
+    public setTree(): void {
+        this._tree = this._makeTree(this._data);
+    }
+
+    public setTreeReduced(): void {
+        this._tree = this._makeTreeReduced(this._data);
+    }
+
+    public getTree(): TreeCode[] {
+        return this._tree;
+    }
 }
 
 const tree = new TreeConvertor(data);
 tree.setTree();
+tree.setTreeReduced();
 console.log(tree.getTree());
