aca48914c9bd4c6b6cbe191637b0d24d202daafc
PavPavv <pavpavdev@gmail.com>
2025-10-04 21:09:44 +0400

    feat: start basic algorithms explanation cheat sheet

--- python/sandbox.py
+++ python/sandbox.py
@@ -15,12 +15,30 @@ class Queue:
         return self.queue
 
 def main():
-    q = Queue()
-    q.enqueue("Jack")
-    q.enqueue("Mag")
-    print(q.get_queue())
-    q.dequeue()
-    print(q.get_queue())
+    # q = Queue()
+    # q.enqueue("Jack")
+    # q.enqueue("Mag")
+    # print(q.get_queue())
+    # q.dequeue()
+    # print(q.get_queue())
+    
+    def binary_search(l, target):
+        low = 0
+        high = len(l) - 1
+
+        while low <= high:
+            mid = (low+high) // 2
+            guess = l[mid]
+
+            if guess == target:
+                return mid
+            elif guess > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return None
+    print(binary_search([1,3,5,7,9], 3))
+    print(binary_search([1,3,5,7,9], -1))
 
 if __name__ == "__main__":
     main()
